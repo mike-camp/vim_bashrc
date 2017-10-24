4827adf5817e69343231753a10377b9e12177935
Ubuntu <ubuntu@ip-172-31-23-236.ec2.internal>
2017-10-24 20:25:13 +0000

    add YCM install instructiosn

--- install_vim.sh
+++ install_vim.sh
@@ -4,3 +4,8 @@ cp .vimrc ~/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 echo :PluginInstall | vim
+
+sudo apt-get install build-essential cmake
+sudo apt-get install python-dev python3-dev
+cd ~/.vim/bundle/YouCompleteMe
+python install.py
